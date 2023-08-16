a71085dccfea46ed8d2dc11a4c5afa66a7041368
SergeyOlkin <4567310Ajiger@gmail.com>
2023-08-17 02:36:27 +0300

    Added changes

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from catalog.views import home, contact
+from catalog.views import home, contact, product_page
 
 urlpatterns = [
     path('', home, name='home'),
     path('contact/', contact, name='contact'),
+    path('product/<int:product_id>/', product_page, name='product'),
 ]
