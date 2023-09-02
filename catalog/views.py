7fb479b6f375ac27520caadbcb01cd0b44894ce9
SergeyOlkin <4567310Ajiger@gmail.com>
2023-09-02 23:41:05 +0300

    Added changes

--- catalog/views.py
+++ catalog/views.py
@@ -1,93 +1,83 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import redirect
 from django.utils.text import slugify
-from django.views import View
 
 from catalog.models import Product
 from .models import BlogPost
 from .forms import BlogPostForm
 
 
-class HomeView(View):
-    @staticmethod
-    def get(request):
-        product_list = Product.objects.all()
-        blog_posts = BlogPost.objects.filter(published=True)  # фильтруем только опубликованные статьи
-        context = {
-            'object_list': product_list,
-            'blog_posts': blog_posts
-        }
-        return render(request, 'catalog/home.html', context)
+class HomeView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    context_object_name = 'object_list'
+
+    def get_queryset(self):
+        return super().get_queryset()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['blog_posts'] = BlogPost.objects.filter(published=True)
+        return context
 
 
-class ContactView(View):
-    @staticmethod
-    def get(request):
-        return render(request, 'catalog/contact.html')
+class ContactView(ListView):
+    template_name = 'catalog/contact.html'
 
 
-class ProductView(View):
-    @staticmethod
-    def get_product_info(product_id):
-        product = Product.objects.get(id=product_id)
-        return product
-
-    @staticmethod
-    def get(request, product_id):
-        product_info = ProductView.get_product_info(product_id)
-        return render(request, 'catalog/product.html', {'product_info': product_info})
+class ProductView(DetailView):
+    model = Product
+    template_name = 'catalog/product.html'
+    context_object_name = 'product_info'
 
 
-class BlogPostCreateView(View):
-    @staticmethod
-    def get(request):
-        form = BlogPostForm()
-        return render(request, 'catalog/blog_post_create.html', {'form': form})
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    form_class = BlogPostForm
+    template_name = 'catalog/blog_create.html'
 
-    @staticmethod
-    def post(request):
-        form = BlogPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            blog_post = form.save(commit=False)
-            blog_post.slug = slugify(blog_post.title)  # формируем slug name из заголовка
-            blog_post.save()
-            return redirect('blog_detail', pk=blog_post.pk)
-        return render(request, 'catalog/blog_post_create.html', {'form': form})
+    def form_valid(self, form):
+        blog_post = form.save(commit=False)
+        blog_post.slug = slugify(blog_post.title)
+        blog_post.save()
+        return redirect('blog_detail', pk=blog_post.pk)
 
 
-class BlogPostDetailView(View):
-    @staticmethod
-    def get(request, pk):
-        blog_post = get_object_or_404(BlogPost, pk=pk)
-        blog_post.views += 1  # увеличиваем счетчик просмотров на 1
-        blog_post.save()  # сохраняем изменения
-        return render(request, 'catalog/blog_post_detail.html', {'blog_post': blog_post})
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+    template_name = 'catalog/blog_post_detail.html'
+    context_object_name = 'blog_post'
+
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        self.object = None
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.views += 1
+        self.object.save()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
 
 
-class BlogPostUpdateView(View):
-    @staticmethod
-    def get(request, pk):
-        blog_post = get_object_or_404(BlogPost, pk=pk)
-        form = BlogPostForm(instance=blog_post)
-        return render(request, 'catalog/blog_post_update.html', {'form': form})
+class BlogPostUpdateView(UpdateView):
+    model = BlogPost
+    form_class = BlogPostForm
+    template_name = 'catalog/blog_update.html'
+    context_object_name = 'blog_post'
 
-    @staticmethod
-    def post(request, pk):
-        blog_post = get_object_or_404(BlogPost, pk=pk)
-        form = BlogPostForm(request.POST, request.FILES, instance=blog_post)
-        if form.is_valid():
-            form.save()
-            return redirect('blog_detail', pk=pk)
-        return render(request, 'catalog/blog_post_update.html', {'form': form})
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog_detail', kwargs={'pk': self.object.pk})
 
 
-class BlogPostDeleteView(View):
-    @staticmethod
-    def get(request, pk):
-        blog_post = get_object_or_404(BlogPost, pk=pk)
-        return render(request, 'catalog/blog_post_delete.html', {'blog_post': blog_post})
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'catalog/blog_delete.html'
+    context_object_name = 'blog_post'
 
-    @staticmethod
-    def post(pk):
-        blog_post = get_object_or_404(BlogPost, pk=pk)
-        blog_post.delete()
-        return redirect('blog_list')
+    def get_success_url(self):
+        return reverse('blog_list')
